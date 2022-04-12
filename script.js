fcb429651699dc08157edfdc4da24a29bd367aa2
gut11 <guto.gomes@hotmail.com>
2022-04-12 14:24:57 -0300

    fixed details

--- script.js
+++ script.js
@@ -7,14 +7,23 @@ let cont = 0;
 
 
 function open_mobile_menu(){
-    if(cont % 2 == 0){
-    menu_container.style.maxHeight = "30em";
-    }
-    else{
+    menu_container.style = "";
+    if(cont % 2 == 0)
+        menu_container.style.maxHeight = "30em";
+    else
         menu_container.style.maxHeight = "0";
-    }
     cont++;
 }
 
 
+function close_mobile_menu_desktop_mode(){
+    if(window.matchMedia("(min-width: 900px)").matches){
+    menu_container.style.transition = "none";
+    menu_container.style.maxHeight = "0";
+    cont = 0;
+    }
+}
+
+
+window.addEventListener("resize", close_mobile_menu_desktop_mode);
 mobile_button.addEventListener("click", open_mobile_menu);
